5745e4213fba1effb8234b5e3260cf10d8d0e9ed
JF41170819 <31319921+JF41170819@users.noreply.github.com>
2018-05-08 07:37:30 -0300

    Add files via upload

---
+++ Clase 6/EntidadesClase6/EntidadesClase6/Paleta.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesClase6
+{
+    class Paleta
+    {
+        private Tempera[] colores_Array;
+        private int CantidadMaximaColores;
+
+        private Paleta(int cantidad)
+        {
+            this.CantidadMaximaColores = cantidad;
+            this.colores_Array = new Tempera[cantidad];
+        }
+
+        private Paleta()
+            : this(5)
+        {
+
+        }
+
+        public static implicit operator Paleta(int cantidad)
+        {
+            Paleta objPaleta = new Paleta(cantidad);
+            return objPaleta;
+        }
+
+        private string Mostrar()
+        {
+            string retorno = "Cantidad maxima de colores: " + this.CantidadMaximaColores + "\n";
+
+            for (int i = 0; i < this.CantidadMaximaColores; i++)
+            {
+                if (this.colores_Array.GetValue(i) != null)
+                {
+                    retorno += Tempera.Mostrar(this.colores_Array[i]) + "\n";
+                }
+            }
+
+            return retorno;
+
+        }
+
+        public static explicit operator string(Paleta objPaleta)
+        {
+            return objPaleta.Mostrar();
+        }
+
+        public static bool operator ==(Paleta objPaleta, Tempera objTempera)
+        {
+            bool retorno = false;
+
+            for (int i = 0; i < objPaleta.CantidadMaximaColores; i++)
+            {
+                if (objPaleta.colores_Array.GetValue(i) != null)
+                {
+                    if (objPaleta.colores_Array[i] == objTempera)
+                    {
+                        retorno = true;
+                        break;
+                    }
+                }
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Paleta objPaleta, Tempera objTempera)
+        {
+            return !(objPaleta == objTempera);
+        }
+
+        public static Paleta operator +(Paleta objPaleta, Tempera objTempera)
+        {
+            if (objPaleta == objTempera)
+            {
+                int indice1 = objPaleta.ObtenerIndice(objTempera);
+
+                objPaleta.colores_Array[indice1] += objTempera;
+            }
+            else if (objPaleta != objTempera)
+            {
+                int indice2 = objPaleta.ObtenerIndice();
+
+                objPaleta.colores_Array[indice2] += objTempera;
+            }
+
+            return objPaleta;
+        }
+
+        private int ObtenerIndice()
+        {
+            int indice = -1;
+
+            for (int i = 0; i < this.CantidadMaximaColores; i++)
+            {
+                if ((object)this.colores_Array[i] == null) //this.colores_Array.GetValue(i)
+                {
+                    indice = i;
+                    break;
+                }
+            }
+
+            return indice;
+        }
+
+        private int ObtenerIndice(Tempera objTempera)
+        {
+            int indice = -1;
+
+            for (int i = 0; i < this.CantidadMaximaColores; i++)
+            {
+                if (this.colores_Array[i] == objTempera)
+                {
+                    indice = i;
+                    break;
+                }
+            }
+
+            return indice;
+        }
+
+        public static Paleta operator -(Paleta objPaleta, Tempera objTempera)
+        {
+            if (objPaleta == objTempera)
+            {
+                int indice1 = objPaleta.ObtenerIndice(objTempera);
+
+                if (indice1 > 0)
+                {
+                    objPaleta.colores_Array[indice1] += -(objTempera);
+
+                    if (objPaleta.colores_Array[indice1] <= 0)
+                    {
+                        objPaleta.colores_Array[indice1] = null;
+                    }
+                }
+            }
+
+            return objPaleta;
+
+        }
+
+        //Operador sobrecarga +, sumar las paletas.
+
+        public static Paleta operator +(Paleta objPaleta, Tempera objTempera)
+        {
+            if (objPaleta == objTempera)
+            {
+                int indice1 = objPaleta.ObtenerIndice(objTempera);
+
+                if (indice1 > 0)
+                {
+                    objPaleta.colores_Array[indice1] += (objTempera);
+
+                    if (objPaleta.colores_Array[indice1] <= 0)
+                    {
+                        objPaleta.colores_Array[indice1] = null;
+                    }
+                }
+            }
+
+            return objPaleta;
+        }
+
+    }
+}
---
+++ Clase 6/EntidadesClase6/EntidadesClase6/Tempera.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesClase6
+{
+    public class Tempera
+    {
+        private ConsoleColor color;
+        private string marca;
+        private int cantidad;
+
+        public Tempera(ConsoleColor color, string marca, int cantidad)
+        {
+            this.color = color;
+            this.marca = marca;
+            this.cantidad = cantidad;
+
+        }
+
+        private string Mostrar()
+        {
+            return this.color + "-" + this.marca + "-" + this.cantidad;
+        }
+
+        public static string Mostrar(Tempera objTempera)
+        {
+            return objTempera.Mostrar();
+        }
+
+        public static bool operator ==(Tempera objTempera1, Tempera objTempera2)
+        {
+            bool retorno = false;
+
+            if (objTempera1.color == objTempera2.color && objTempera1.marca == objTempera2.marca)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Tempera objTempera1, Tempera objTempera2)
+        {
+            return !(objTempera1 == objTempera2);
+        }
+
+        public static implicit operator int(Tempera objTempera1)
+        {
+            return objTempera1.cantidad;
+        }
+
+        public static explicit operator string(Tempera objTempera1)
+        {
+            return objTempera1.Mostrar();
+        }
+
+        public static Tempera operator +(Tempera objTempera1, int cantidad)
+        {
+            objTempera1.cantidad +=cantidad;
+            return objTempera1;
+        }
+
+        public static Tempera operator +(Tempera objTempera1, Tempera objTempera2)
+        {
+            if (objTempera1 == objTempera2)
+            {
+                objTempera1 += objTempera2;
+            }
+
+            return objTempera1;
+        }
+    }
+}
